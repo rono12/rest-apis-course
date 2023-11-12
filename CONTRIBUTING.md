dad3e6f99a3166c724e7f7feadf82d4916266fcd
Ron Oved <ron.oved2@gmail.com>
2023-11-12 02:04:12 +0200

    adding modifications

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,5 +1,6 @@
 ## How to run docker locally
 
 ```
+docker build -t IMAGE_NAME .
 docker run -dp 5005:5000 -w /app -v "$(pwd):/app" IMAGE_NAME sh -c "flask run --host 0.0.0.0"
 ```
--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
 
 import os
 
@@ -17,6 +18,8 @@ from resources.user import blp as UserBlueprint
 
 def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()
+    
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
